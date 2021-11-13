53b5451c35047ee928f3376bd5639035cbbdcebf
Nilesh9492 <nilesh94041@gmail.com>
2021-11-13 10:20:19 +0530

    Create Payroll Service Database

---
+++ EmployeePayrollADO/EmployeeRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Sql
+using System.Text;
+
+namespace EmployeePayrollADO
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=paroll_service;Integrated Security=True";
+        SqlConnection sqlconnection = new SqlConnection(connectionString);
+    }
+}
---
+++ EmployeePayrollADO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeePayrollADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Payroll ADO!");
+            EmployeeRepo repository = new EmployeeRepo();
+        }
+    }
+}
